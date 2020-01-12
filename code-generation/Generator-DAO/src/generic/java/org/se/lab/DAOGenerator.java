5ea74ba9d5d9c5f25be3225ebd0f3cd9ea4cdfdd
Patrick Kainz <patrick@kainz.sh>
2020-01-12 10:53:48 +0100

    added method to ease generation with nonexistant paths

--- code-generation/Generator-DAO/src/generic/java/org/se/lab/DAOGenerator.java
+++ code-generation/Generator-DAO/src/generic/java/org/se/lab/DAOGenerator.java
@@ -4,6 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Date;
 
 import org.se.lab.metamodel.MEntity;
@@ -12,119 +14,109 @@ import org.se.lab.metamodel.MProperty;
 import org.se.lab.metamodel.MString;
 import org.se.lab.metamodel.MType;
 
-public abstract class DAOGenerator
-	implements EntityVisitor
-{
+public abstract class DAOGenerator implements EntityVisitor {
 	protected StringBuilder code;
-	
-	public DAOGenerator()
-	{
+
+	public DAOGenerator() {
 		code = new StringBuilder();
 	}
-	
-	public String generate(MEntity entity, String packageName)
-	{
+
+	public String generate(MEntity entity, String packageName) {
 		generateComment();
 		setPackageName(packageName);
 		visit(entity);
 		return code.toString();
 	}
-	
-	public void writeCodeTo(MEntity entity, String packageName, String filename, String path)
-	{
-		String s = generate(entity, packageName);		
-		
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path, filename)))) 
-		{
-		    writer.write(s, 0, s.length());
-		} 
-		catch (IOException e) 
-		{
+
+	public void writeCodeTo(MEntity entity, String packageName, String filename, String path) {
+
+		createPathIfNotExists(path);
+
+		String s = generate(entity, packageName);
+
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path, filename)))) {
+			writer.write(s, 0, s.length());
+		} catch (IOException e) {
 			throw new IllegalStateException("Can't write " + filename + " to " + path + "!", e);
 		}
 	}
-	
-	
+
+	private void createPathIfNotExists(String path) {
+		if (!Files.exists(Paths.get(path))) {
+			try {
+				Files.createDirectories(Paths.get(path));
+			} catch (IOException e) {
+				throw new IllegalStateException("Can't create " + path + "!", e);
+			}
+		}
+	}
+
 	/*
 	 * Property: packageName
 	 */
 	private String packageName;
-	public String getPackageName()
-	{
+
+	public String getPackageName() {
 		return packageName;
 	}
-	public void setPackageName(String packageName)
-	{
-		if(packageName == null)
+
+	public void setPackageName(String packageName) {
+		if (packageName == null)
 			throw new IllegalArgumentException("Package name is null!");
 		this.packageName = packageName;
 	}
 
-	public void generateComment()
-	{
+	public void generateComment() {
 		Date now = new Date();
 		code.append("// This file has been generated automatically at ");
 		code.append(now).append("\n");
 		code.append("// Don't modify it manually!\n\n");
 	}
-	
-	public void generatePropertyName(String name)
-	{
+
+	public void generatePropertyName(String name) {
 		code.append(Character.toUpperCase(name.charAt(0)));
-		code.append(name.substring(1,name.length()));		
+		code.append(name.substring(1, name.length()));
 	}
-	
-	
+
 	@Override
-	public void visit(MEntity entity)
-	{
+	public void visit(MEntity entity) {
 		// code generation
-		
+
 		// navigation
-		for(MProperty p : entity.getProperties())
-		{
+		for (MProperty p : entity.getProperties()) {
 			visit(p);
 		}
 	}
 
 	@Override
-	public void visit(MProperty property)
-	{
+	public void visit(MProperty property) {
 		// code generation
-		
+
 		// navigation
 		visit(property.getType());
 	}
 
 	@Override
-	public void visit(MType type)
-	{
+	public void visit(MType type) {
 		// navigation
-		if(type instanceof MInteger)
-		{
-			visit((MInteger)type);
-		}
-		else if(type instanceof MString)
-		{
-			visit((MString)type);
-		}
-		else
-		{
+		if (type instanceof MInteger) {
+			visit((MInteger) type);
+		} else if (type instanceof MString) {
+			visit((MString) type);
+		} else {
 			throw new IllegalStateException("Unknown MType!");
-		}		
+		}
 	}
 
 	@Override
-	public void visit(MInteger i)
-	{
+	public void visit(MInteger i) {
 		// code generation
 		code.append("int");
 	}
 
 	@Override
-	public void visit(MString s)
-	{
+	public void visit(MString s) {
 		// code generation
-		code.append("String");		
+		code.append("String");
 	}
 }
